1b9b60e4a14747d28122e776547ef5553ce674e3
Alberto Varela <alberto@berriart.com>
2017-02-17 01:33:31 +0100

    Adding neew version

--- src/azcosts.js
+++ src/azcosts.js
@@ -6,7 +6,7 @@ let program = require('commander');
 
 // Parse args
 program
-    .version('1.0.0')
+    .version('1.0.1')
     .command('init', 'Initialize Azcosts project. Creates an azcosts.json config file.')
     .command('get', 'Get Azure costs.')
     .parse(process.argv);
